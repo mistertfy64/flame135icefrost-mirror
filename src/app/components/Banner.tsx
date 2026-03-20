2bb618728b200c2c5a279a97b2a666754905741f
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-20 10:55:36 +0700

    feat(ui): banner

---
+++ src/app/components/Banner.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Banner(){
+  return (
+    <div className="w-full bg-rose-500 flex justify-center items-center h-[30vh]">
+      <div className="w-[75%] text-white">
+        <span className="text-4xl">Find your perfect stay!</span>
+        <br/>
+        <span className="text-xl">Book amazing hotels across Thailand with ease!</span>
+      </div>
+    </div>
+  )
+}
