ef6391fe55e80744982f41928d353a57511e1dec
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-20 17:51:13 +0700

    chore(ui): use prettier on file format

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,16 +1,18 @@
 import FrontPageSearch from "./FrontPageSearch";
 
-export default function Banner(){
+export default function Banner() {
   return (
-    <div className="w-full bg-rose-500 flex justify-center items-center h-[30vh]">
-      <div className="w-[75%] text-white">
+    <div className="w-full bg-primary flex justify-center items-center h-[30vh]">
+      <div className="w-[var(--general-width)] text-white">
         <span className="text-4xl">Find your perfect stay!</span>
-        <br/>
-        <span className="text-xl">Book amazing hotels across Thailand with ease!</span>
-        <br/>
-        <br/>
-        <FrontPageSearch/>
+        <br />
+        <span className="text-xl">
+          Book amazing hotels across Thailand with ease!
+        </span>
+        <br />
+        <br />
+        <FrontPageSearch />
       </div>
     </div>
-  )
+  );
 }
