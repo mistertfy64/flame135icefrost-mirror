19d1ee62f2bcaec6e84957cd32c7b5544957b8ab
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-20 17:52:20 +0700

    fix(ui): make button look more natural (`mr 4` to `mr 8`)

--- src/app/components/FrontPageSearch.tsx
+++ src/app/components/FrontPageSearch.tsx
@@ -1,11 +1,12 @@
-
 "use client";
 
 export default function FrontPageSearch() {
   return (
     <div className="flex flex-col md:flex-row md:w-[42.5%] md:justify-between">
-      <input className="bg-white text-black p-[8px] text-xl rounded-md my-[4px] md:mr-[4px] md:my-[0px] md:flex-grow"></input>
-      <button className="bg-emerald-700 rounded-md p-[8px] text-xl">Search</button>
+      <input className="bg-white text-black p-[8px] text-xl rounded-md my-[4px] md:mr-[8px] md:my-[0px] md:flex-grow"></input>
+      <button className="bg-emerald-700 rounded-md p-[8px] text-xl">
+        Search
+      </button>
     </div>
-  )
+  );
 }
