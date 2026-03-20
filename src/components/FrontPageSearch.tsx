9a0c289cc88c97d459b1fe383187596869278b4f
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-20 19:11:11 +0700

    fix(ui): add placeholder text to `input`

--- src/components/FrontPageSearch.tsx
+++ src/components/FrontPageSearch.tsx
@@ -3,7 +3,10 @@
 export default function FrontPageSearch() {
   return (
     <div className="flex flex-col md:flex-row md:w-[42.5%] md:justify-between">
-      <input className="bg-white text-black p-[8px] text-xl rounded-md my-[4px] md:mr-[8px] md:my-[0px] md:flex-grow"></input>
+      <input
+        placeholder="Search for a hotel..."
+        className="bg-white text-black p-[8px] text-xl rounded-md my-[4px] md:mr-[8px] md:my-[0px] md:flex-grow"
+      ></input>
       <button className="cursor-pointer bg-emerald-700 rounded-md px-[16px] py-[8px] text-xl">
         Search
       </button>
