7205bcc8072775e66bbd3d0afef6d9beefae012a
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-20 23:14:15 +0700

    feat(ui): add theme

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Sarabun", "sans-serif"].join(",")
+  }
+});
+
+export default theme;
