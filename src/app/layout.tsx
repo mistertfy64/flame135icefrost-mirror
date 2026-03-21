b77774d5a2b4b49246e12412693310bb30488bcd
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-21 15:27:44 +0700

    fix(auth): fix NextAuth errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,23 +10,30 @@ const sarabun = Sarabun({
 
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/theme";
+import NextAuthProvider from "@/providers/NextAuthProvider";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./api/auth/[...nextauth]/authOptions";
 
 export const metadata: Metadata = {
   title: "Group135 Frontend",
   description: "someone changed the group name to something uncool :("
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const nextAuthSession = await getServerSession(authOptions);
+
   return (
     <html lang="en">
       <head></head>
       <body className={`${sarabun.className} bg-stone-50`}>
-        <Header />
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        <NextAuthProvider session={nextAuthSession}>
+          <Header />
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
