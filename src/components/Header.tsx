b21098330efef0e87e7270eb2afcab73ae294e19
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-23 12:31:46 +0700

    feat(auth): add adaptable header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,57 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 
-export default function TopMenu() {
+export default async function TopMenu() {
+  const session = await getServerSession(authOptions);
+
   return (
     <header className="w-full flex justify-center py-[4px] bg-white">
       <div className="w-[var(--general-width)] h-[4vh] flex justify-between items-center">
         <div>
           <Link href="/">Group135</Link>
         </div>
-        <div>
-          <Link className="mx-[8px]" href="/hotels">
-            Hotels
-          </Link>
-          <Link
-            className="mx-[8px] text-primary underline cursor-pointer"
-            href="/login"
-          >
-            Login
-          </Link>
-          {/** TODO: Make this more consistent? */}
-          <Link className="ml-[8px] text-text-primary " href="/register">
-            <button className="cursor-pointer rounded-md bg-primary text-text px-[16px] py-[4px]">
-              Register
-            </button>
-          </Link>
-        </div>
+        {session ? (
+          <div>
+            <Link className="mx-[8px]" href="/hotels">
+              Hotels
+            </Link>
+            <Link
+              className="mx-[8px] text-primary underline cursor-pointer"
+              href="/logout"
+            >
+              Logout
+            </Link>
+            <Link
+              className="mx-[8px] text-primary underline cursor-pointer"
+              href="/me"
+            >
+              {"Unknown User Name"}
+            </Link>
+            <Link className="ml-[8px] text-text-primary " href="/register">
+              <button className="cursor-pointer rounded-md bg-primary text-text px-[16px] py-[4px]">
+                My Bookings
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <Link className="mx-[8px]" href="/hotels">
+              Hotels
+            </Link>
+            <Link
+              className="mx-[8px] text-primary underline cursor-pointer"
+              href="/login"
+            >
+              Login
+            </Link>
+            <Link className="ml-[8px] text-text-primary " href="/register">
+              <button className="cursor-pointer rounded-md bg-primary text-text px-[16px] py-[4px]">
+                Register
+              </button>
+            </Link>
+          </div>
+        )}
       </div>
     </header>
   );
