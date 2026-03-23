759ef6b8a76348b4963a8b9dfb6873969d99d002
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-23 13:38:09 +0700

    feat(auth): add `getMe` function

---
+++ src/libs/getMe.tsx
@@ -0,0 +1,12 @@
+export default async function getMe(token: string) {
+  const API_BASE_URL = process.env.BACKEND_BASE_URL;
+
+  const response = await fetch(`${API_BASE_URL}/api/v1/auth/me`, {
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${token}`
+    }
+  });
+
+  return await response.json();
+}
